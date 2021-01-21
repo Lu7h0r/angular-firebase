bb66303e2b532fc1557820b2662022475213b148
Alex Bautista <lex.magno@gmail.com>
2021-01-21 08:03:12 -0500

    fire config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+    firebaseConfig: {
+    apiKey: 'AIzaSyCJIVT1TQTddIHO121NZ1JT8k_2gSvsIPg',
+    authDomain: 'crud-empleados-2f28c.firebaseapp.com',
+    projectId: 'crud-empleados-2f28c',
+    storageBucket: 'crud-empleados-2f28c.appspot.com',
+    messagingSenderId: '784844941242',
+    appId: '1:784844941242:web:df53c84aa52ef3d00d9f83',
+  },
 };
